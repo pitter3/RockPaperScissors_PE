188dcec3612a34be1204d01708e3ceb4bad9cee0
Patrick Eitel <patrickeitel12@gmail.com>
2023-08-03 15:39:30 -0700

    Remove win property from game object. Add a createGame function

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
+// DATA MODEL
 var humanScore = 0;
 var robotScore = 0;
+
+
+// GAME LOGIC
+
+
 
 function createPlayer(name, selection) {
   var player = {
     name: name,
     selection: selection,
-    wins: 0,
   }
   return player
 }
+
+var humanPlayer = createPlayer("Human", "Rock");
+var robotPlayer = createPlayer("Robot", "Scissors");
+
+
+function createGame(human, robot) {
+  var game = {
+    human: {
+      selection: human.selection,
+      gameWon: false
+    },
+    robot: {
+      selection: robot.selection,
+      gameWon: false,
+    }
+  }
+  return game
+}
+
+var game = createGame(humanPlayer, robotPlayer);
